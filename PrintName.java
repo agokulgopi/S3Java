734d86c3001a54b5ab0771572df46dc5e942aa63
agokulgopi <gokulgopi2106@gmail.com>
2022-11-06 16:29:29 +0530

    Program2 Name,Age,Address

---
+++ PrintName.java
@@ -0,0 +1,7 @@
+public class PrintName {
+    public static void main(String arg[]){
+        System.out.println("Name:Gokul Gopi");
+        System.out.println("Age:20");
+        System.out.println("Address:Chittulliparambil");
+    }
+}

4c1ac1fabaa37be5dd69783db869ee9b779dd660
agokulgopi <gokulgopi2106@gmail.com>
2022-11-06 16:33:40 +0530

    Program3

---
+++ StudentMarklist.java
@@ -0,0 +1,9 @@
+public class StudentMarklist {
+     public static void main(String[] args) {
+        System.out.println("English=100");
+        System.out.println("Physics=90");
+        System.out.println("Chemistry=94");
+        System.out.println("Computer Science=100");
+        System.out.println("Mathematics=80");
+    }
+}

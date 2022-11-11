51544e5e0dd9cf644f84e9e2797b4f7ff179a64f
agokulgopi <gokulgopi2106@gmail.com>
2022-11-11 20:07:51 +0530

    Program10  Factorial Modified

--- Factorial.java
+++ Factorial.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Factorial {
     public static void main(String[] args) {
-        int digit,fact=1;
+        int fact=1;
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the Number:");
         int num=sc.nextInt();
